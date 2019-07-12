ffd14ee6725b6a697f990d97f580b180613d41e0
akki8092 <52438858+akki8092@users.noreply.github.com>
2019-07-12 11:52:14 +0900

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//JS メニュー操作
+
+(() => {
+  'use strict';
+
+  var show = document.getElementById('show');
+  var hide = document.getElementById('hide');
+
+  show.addEventListener('click', function() {
+    document.body.className = 'menu-open';
+  });
+
+  hide.addEventListener('click', function() {
+    document.body.className = '';
+  });
+})();
